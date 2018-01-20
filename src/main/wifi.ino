21cc28729183cd29688fbe1b9e6ad190b87f644b
Cayon <eti.cayon@hotmail.fr>
2018-01-20 19:07:10 +0100

    WIFI ok

--- src/main/wifi.ino
+++ src/main/wifi.ino
@@ -9,13 +9,8 @@ WiFiClass WiFi(&SPI_3, PE0, PE1, PE8, PB13);
 WiFiClient wifiClient;
 char ssid[] = "24HDUCODE";
 char pass[] = "2018#24hcode!";
-uint8_t http[1024];               // http buffer to send
 int status = WL_IDLE_STATUS;
-
-
-//PubSubClient client("24hducode.spc5studio.com", 1883, callback, wifiClient);
-
-PubSubClient client(wifiClient);
+PubSubClient client("24hducode.spc5studio.com", 1883, callback, wifiClient);
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -34,61 +29,43 @@ void setup_wifi()
     Serial.println("WiFi shield not present");
     while (true);          // don't continue
   }
- 
+
+ WiFi.begin(ssid, pass);
   // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);                   // print the network name (SSID);
-
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-    // wait 10 seconds for connection:
-    delay(10000);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  printWifiStatus();                        // you're connected now, so print out the status
+  
   Serial.println("Wifi ok");
-  client.setServer("24hducode.spc5studio.com", 1883);
-  //client.setCallback(callback);
-  while(client.connected()==0) {
-    Serial.println("Tentative connexion");
-    client.connect("teamF","Aspicot", "G9375V72");
+  
+  if (client.connect("teamF","Aspicot", "G9375V72")) {
+    Serial.println("Connected to MQTT broker");
+    
+    if (client.publish("24hcode/teamF/284k0/device2broker","Hello from Aspicot")) {
+      Serial.println("Publish ok");
+    }
+    else {
+      Serial.println("Publish failed");
+    }
+
+    if (client.subscribe("24hcode/teamF/284k0/broker2device")) {
+      Serial.println("Subscribe ok");
+    }
+    else {
+      Serial.println("Subscribe failed");
+    }
+    
   }
-  Serial.println("Connected to brocker");
-  Serial.println(client.connected());
-  Serial.println("tentative d'envoi");
-  do{
-    Serial.println("Sending to brocker"); 
-  }while(client.publish("24hcode/teamF/284k0/device2broker","Hello from Aspicot")==0);
+  else {
+    Serial.println("MQTT connect failed");
+    Serial.println("Will reset and try again...");
+    abort();
+  }
 
-  Serial.println("Published done");
-  client.subscribe("24hcode/teamF/284k0/broker2device");
 }
 
-void loop_wifi()
-{
-  //client.loop();
+void loop_wifi(){
+  client.loop();
 }
-
-
-
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
-  // print where to go in a browser:
-  Serial.print("To see this page in action, open a browser to http://");
-  Serial.println(ip);
-}
-
 
