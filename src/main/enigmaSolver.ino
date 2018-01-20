d34d1539f4b5a3f551b4da7fd8f8c73f1d3e4976
Doktor <ThibDu50@hotmail.fr>
2018-01-20 21:09:01 +0100

    Add function dj - A3

--- src/main/enigmaSolver.ino
+++ src/main/enigmaSolver.ino
@@ -1,9 +1,32 @@
-void circuit(){
+#define C  262
+#define C# 277
+#define D  294
+#define D# 311
+#define E  330
+#define F  349
+#define F# 370
+#define G  392
+#define G# 415
+#define A  440
+#define A# 466
+#define B  494
+
+
+void circuit(){//A2
   
 }
 
-void dj(){
-  
+void dj(char music[]){//A3 A valider
+  int note;
+  for (int k=0; k<strlen(music);k++){
+    note = null;
+    note = music[k];
+    if(k < strlen(music) && music[k+1]=="#"){
+      note = note+"#";
+      k++;
+    }
+    tone(PIN_BUZZER, note);
+  }
 }
 
 void 360(){
@@ -14,9 +37,12 @@ void swipe(){
   
 }
 
-String crypto(String enonce){
+String crypto(char enonce[]){//A revoir !
   String decode;
-  
+//  for(int i=0;i<26;i++){
+//    for(int j;j<strlen(enonce);j++){
+//    enonce
+//  }
   return decode;
 }
 
@@ -25,7 +51,9 @@ void swipe(){
 }
 
 void spydj(String enonce,String key){
+//crypt music
 
+//play music
 }
 
 void ble(){
