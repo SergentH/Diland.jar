cee3fc101087836e648f0be2c1bbb64480762332
Cayon <eti.cayon@hotmail.fr>
2018-01-20 13:19:16 +0100

    Fonction vitesse

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,6 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-
+  setup_move();
+  for(int i=0;i<100;i++){
+    move(i,i);
+    delay(100);
+  }
+  move(0,0);
 }
 
 void loop() {
--- src/main/move.ino
+++ src/main/move.ino
@@ -1,1 +1,25 @@
+#include <Servo.h> 
+Servo servoG;
+Servo servoD;
+
+void setup_move(){
+  servoG.attach(6,900,2100);
+  servoD.attach(5,900,2100);
+}
+
+
+void move(int vitG, int vitD){
+  double cmdG = vitG*1200/200+1500;
+  double cmdD = -vitD*1200/200+1500;
+  servoG.write(cmdG);
+  servoD.write(cmdD);
+}
+
+void rotate(int angle){
+  
+}
+
+
+
+
 
