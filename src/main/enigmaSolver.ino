cefbe8d44e936a7282602c3b870c2ce044e538cc
Doktor <ThibDu50@hotmail.fr>
2018-01-20 19:33:28 +0100

    First push, all void functions

---
+++ src/main/enigmaSolver.ino
@@ -0,0 +1,47 @@
+void circuit(){
+  
+}
+
+void dj(){
+  
+}
+
+void 360(){
+  
+}
+
+void swipe(){
+  
+}
+
+String crypto(String enonce){
+  String decode;
+  
+  return decode;
+}
+
+void swipe(){
+  
+}
+
+void spydj(String enonce,String key){
+
+}
+
+void ble(){
+  
+}
+
+String magneton(){
+  
+  return "porte"+numPorte;
+}
+
+String memory(enonce){//ultra HARDCORE
+  
+}
+
+void victoire(){//here, yolo mode
+  
+}
+
